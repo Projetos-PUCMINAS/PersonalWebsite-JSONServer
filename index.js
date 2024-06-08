3af3181681f3d281ce79925ecadf3b03566b1c87
marcosalberto <159904717+marcosffp@users.noreply.github.com>
2024-06-08 18:49:13 +0000

    configuração do json

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
-// Trabalho Interdisciplinar 1 - Aplicações Web
-//
-// Esse módulo implementa uma API RESTful baseada no JSONServer
-// O servidor JSONServer fica hospedado na seguinte URL
-// https://jsonserver.rommelpuc.repl.co/contatos
-//
-// Para montar um servidor para o seu projeto, acesse o projeto 
-// do JSONServer no Replit, faça o FORK do projeto e altere o 
-// arquivo db.json para incluir os dados do seu projeto.
-//
-// URL Projeto JSONServer: https://replit.com/@rommelpuc/JSONServer
-//
-// Autor: Rommel Vieira Carneiro
-// Data: 03/10/2023
+const axios = require('axios');
+const fs = require('fs');
 
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('./db/db.json')
+async function getUserData(username) {
+  try {
+    const response = await axios.get(`https://api.github.com/users/marcosffp`);
+    return response.data;
+  } catch (error) {
+    console.error('Erro ao obter dados do usuário:', error);
+    return null;
+  }
+}
 
-// Para permitir que os dados sejam alterados, altere a linha abaixo
-// colocando o atributo readOnly como false.
-const middlewares = jsonServer.defaults()
+async function main() {
+  const username = 'marcosffp'; // Define o username do usuário do GitHub que você deseja obter
+  const userData = await getUserData(username);
 
-server.use(middlewares)
-server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server está em execução!')
-})
+  if (userData) {
+    // Cria um objeto para armazenar os dados do usuário
+    const githubUser = {
+      id: userData.id,
+      login: userData.login,
+      name: userData.name,
+      bio: userData.bio,
+      location: userData.location,
+      followers: userData.followers,
+      following: userData.following,
+      public_repos: userData.public_repos,
+      public_gists: userData.public_gists,
+    };
+
+    // Converte o objeto para JSON
+    const json = JSON.stringify(githubUser, null, 2);
+
+    // Cria um arquivo JSON e escreve os dados nele
+    fs.writeFileSync('./db/db.json', json);
+
+    console.log('Dados do GitHub adicionados ao arquivo github_user.json com sucesso!');
+  } else {
+    console.log('Usuário não encontrado no GitHub');
+  }
+}
+
+main();
